7b81f21afabbc2a239c40b8fe7ad0d9f442c5af1
peterjinchristiansen <82626937+peterjinchristiansen@users.noreply.github.com>
2021-09-30 18:17:07 -0600

    Improved README Generator

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,11 @@ const fs = require('fs');
 inquirer.prompt([
     {
         type: 'input',
+        name: 'intro',
+        message: 'Please enter an answer to the following questions to generate your README. Any irrelevant question you may leave blank.'
+    },
+    {
+        type: 'input',
         name: 'title',
         message: 'What is the title of your project?'
     },
@@ -21,12 +26,17 @@ inquirer.prompt([
     },
     {
         type: 'input',
+        name: 'link',
+        message: 'If applicable, provide a link to the application',
+    },
+    {
+        type: 'input',
         name: 'usage',
         message: 'What does the user need to know about using the repo?'
     },
     {
         type: 'input',
-        name: 'contribution',
+        name: 'contributing',
         message: 'How can the user contribute to the repo?'
     },
     {
@@ -63,6 +73,16 @@ inquirer.prompt([
         type: 'input',
         name: 'email',
         message: 'What is your e-mail address?'
+    },
+    {
+        type: 'input',
+        name: 'screenshot',
+        message: 'If applicable, provide a screenshot to the application',
+    },
+    {
+        type: 'input',
+        name: 'walkthrough',
+        message: 'If applicable, provide a link to a video walkthrough for your application',
     }
 ]).then(data => {
     fs.writeFile('README.md', generate(data), function() {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,12 @@
-function licenseImage(data) {
+function renderTitle(data) {
+    if(!data.title) {
+        return "No Title Entered"
+    } else {
+        return `## ${data.title}`;
+    }
+}
+
+function renderLicenseImage(data) {
     switch(data.license) {
         case 'Apache':
             return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
@@ -23,80 +31,261 @@ function licenseImage(data) {
         case 'Zlib':
             return `[![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)`;
         case 'None':
-            return null;
+            return "";
     }
-  }
+}
+
+function renderDescription(data) {
+    if(!data.description) {
+        return "";
+    } else {
+        return `
+## Description
+
+    ${data.description}
+
+---
+    `;
+    }
+}
+
+function renderTableOfContents(data) {
+
+    var installationTOC = "";
+    var usageTOC = "";
+    var contributingTOC = "";
+    var testsTOC = "";
+    var questionsTOC = "";
+    var licenseTOC = "";
+    var screenshotTOC = "";
+    var walkthroughTOC = "";
+
+    if(data.installation) {
+        installationTOC = '[Installation](#installation)'
+    }
+
+    if(data.usage || data.link) {
+        usageTOC = '[Usage](#usage)'
+    }
+
+    if(data.contributing) {
+        contributingTOC = '[Contributing](#contributing)'
+    }
+
+    if(data.tests) {
+        testsTOC = '[Tests](#tests)'
+    }
+
+    if(data.email || data.username) {
+        questionsTOC = '[Questions](#questions)'
+    }
+
+    if(data.license) {
+        licenseTOC = '[License](#license)'
+    }
+
+    if(data.screenshot) {
+        screenshotTOC = '[Screenshot](#screenshot)'
+    }
+
+    if(data.walkthrough) {
+        walkthroughTOC = '[Walkthrough](#walkthrough)'
+    }
+
+    return `
+    
+## Table of Contents
+
+${installationTOC}
+
+${usageTOC}
+
+${contributingTOC}
+
+${testsTOC}
+
+${questionsTOC}
+
+${licenseTOC}
+
+${screenshotTOC}
+
+${walkthroughTOC}
+
+---
+
+    `;
+}
+
+function renderInstallation(data) {
+    if(!data.installation) {
+        return "";
+    } else {
+        return `
+## Installation
+
+To install your dependencies, you will be using the following command:
+
+    ${data.installation}
+
+---
+
+    `;
+    }
+}
+
+function renderUsage(data) {
+
+    var usageLink = "";
+    var usageOther = "";
+
+    if(data.link) {
+        usageLink = `Link to deployed application: ${data.link}`;
+    }
+
+    if(data.usage) {
+        usageOther = data.usage;
+    }
+
+    return `
+
+## Usage
+    
+${usageLink}
+
+${usageOther}
+
+---
+
+    `;
+
+}
+
+function renderContributing(data) {
+    if(!data.contributing) {
+        return "";
+    } else {
+        return `
+
+## Contributing
+
+${data.contributing}
+
+---
+        `;
+    }
+}
+
+function renderTests(data) {
+    if(!data.tests) {
+        return "";
+    } else {
+        return `
+
+## Tests
+        
+To test the application, you will be using the following command:
+
+    ${data.tests}
+
+---
+
+        `;
+    }
+}
+
+function renderQuestions(data) {
+
+    var emailBody = "";
+    var githubBody = "";
+
+    if(!data.email && !data.username) {
+        return "";
+    }
+
+    if(data.email) {
+        emailBody = `If you have any questions, you can email me at ${data.email}. `;
+    }
+
+    if(data.username) {
+        githubBody = `You can access my other projects at https://github.com/${data.username}.`
+    }
+
+    return `
+    
+## Questions
+
+${emailBody}${githubBody}
+
+
+---
+    `;
+
+}
+
+function renderScreenshot(data) {
+    if(data.screenshot) {
+        return `
+
+## Screenshot
+
+${data.screenshot}
+
+---
+        `;
+    } else {
+        return "";
+    }
+}
+
+function renderWalkthrough(data) {
+    if(data.walkthrough) {
+        return `
+
+## Walkthrough
+
+${data.walkthrough}
+
+---
+        `;
+    } else {
+        return "";
+    }
+}
+
   
-  function renderLicenseSection(data) {
+function renderLicense(data) {
     if(data.license === 'None') {
         return "";
     } else {
-        return `---
-  ## License
-  
-  This project has been licensed by ${data.license}.
-  
+        return `
+
+## License
+
+This project has been licensed by ${data.license}.
+
+---
         `;
     }
-  }
+}
   
   
   
   
   
-  function generateMarkdown(data) {
-    return `# ${data.title}  ${licenseImage(data)}
-  
-  ## Description
-  
-    ${data.description}
-  
-  ## Table of Contents
-  
-  [Installation](#installation)
-  
-  [Usage](#usage)
-  
-  [Contributing](#contributing)
-  
-  [Tests](#tests)
-  
-  [Questions](#questions)
-  
-  ## Installation
-  
-  To install your dependencies, you will be using the command:
-  
-    ${data.installation}
-  
-  ---
-  
-  ## Usage
-  
-  ${data.usage}
-  
-  ---
-  
-  ## Contributing
-  
-  ${data.contribution}
-  
-  ---
-  
-  ## Tests
-  
-  To test the application, you will be using the command:
-  
-    ${data.tests}
-  
-  ---
-  
-  ## Questions
-  
-  If you have any questions, you can e-mail me at ${data.email}.
-  You can also access my other projects at https://github.com/${data.username}
-  
-  ${renderLicenseSection(data)}
+function generateMarkdown(data) {
+    return `
+${renderTitle(data)}${renderLicenseImage(data)}
+${renderDescription(data)}
+${renderTableOfContents(data)}
+${renderInstallation(data)}
+${renderUsage(data)}
+${renderContributing(data)}
+${renderTests(data)}
+${renderQuestions(data)}
+${renderLicense(data)}
+${renderScreenshot(data)}
+${renderWalkthrough(data)}
     `;
   }
   
